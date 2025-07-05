8cba037d22061f6fbf183c368ae5400e8061231a
FrostFoe <frostfoe@gmail.com>
2025-07-05 15:10:33 +0000

    remove unwanted files folders codes

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,26 +1,3 @@
-export interface Product {
-  id: string;
-  name: string;
-  description: string;
-  price: number;
-  image: string;
-  category: string;
-  rating: number;
-  reviews: number;
-  sizes: string[];
-  colors: { name: string; hex: string }[];
-}
-
-export interface CartItem {
-  id: string;
-  name: string;
-  price: number;
-  image: string;
-  quantity: number;
-  selectedSize: string;
-  selectedColor: { name: string; hex: string };
-}
-
 export interface Project {
   id: string;
   tag: string;
